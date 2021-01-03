e46f576f304e52777ed8fe1c2a9f814c4f5aadcc
Makism Tarasevich <TMaksi@users.noreply.github.com>
2021-01-03 14:43:16 +0300

    parser for preview

---
+++ src/fxp/parser/__init__.py
@@ -0,0 +1,11 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 6.1; Win64;"
+    "x64) AppleWebKit/537.36 (KHTML, like"
+    "Gecko) Chrome/87.0.4280.88 Safari/537.36"
+)
+HOST = "https://www.investing.com"
+PREVIEW_URL = "{}/news/forex-news/{}"
